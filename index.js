867cd90ac05ee9559bc67b444e794763c5b6cc85
Chocoanish <chocoboyanish566@gmail.com>
2023-01-26 19:14:25 +0530

    Mode Toggle Design Completed

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ let collapse = document.getElementsByClassName("collapse")[0],
     profile_name= document.getElementsByClassName("profile")[0].lastElementChild,
     search_input= document.querySelector("#search-input"),
     button_title = document.querySelectorAll('.button > div:nth-child(2)');
-    container_2 = document.getElementsByClassName('container-2')[0];
+    container_2 = document.getElementsByClassName('container-2')[0],
+    Switch = document.getElementsByClassName('switch')[0],
+    toggle = document.querySelector('#toggle'),
+    circle = document.querySelector('#circle'),
+    mode_icon = document.querySelector('#mode-icon');
 
 
 
@@ -18,10 +22,12 @@ collapse.addEventListener("click",()=>{
             search_input.style.display = "unset";
             button_title.forEach(element => {
                 element.style.display = "flex";
+                mode_icon.style.display = "flex";
             });
         }, 250);
         collapse.style.left = "325px";
         container_2.style.width = "75%";
+        Switch.style.margin = "0 8px 0 100px";
     }
     else{
         sidebar.style.width = "60px";
@@ -29,9 +35,22 @@ collapse.addEventListener("click",()=>{
         search_input.style.display = "none";
         button_title.forEach(element => {
             element.style.display = "none";
+            mode_icon.style.display = "none";
         });
         collapse.style.left = "85px";
         container_2.style.width = "91.6%";
+        Switch.style.margin = "0 5px";
     }
 })
 
+// Modes Switch Button
+toggle.addEventListener("click",()=>{
+    if (circle.style.marginLeft == "50%"){
+        circle.style.marginLeft = "0";
+        sidebar.style.backgroundColor = "#fff";
+    }
+    else{
+        circle.style.marginLeft = "50%";
+        sidebar.style.backgroundColor = "#000";
+    }
+})
