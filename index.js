ead7432196a644d38caf7c270806c012fb89aab9
chocoanish <chocoboyanish566@gmail.com>
2023-02-01 20:06:51 +0530

    Download Functionality Completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let collapse = document.getElementsByClassName("collapse")[0],
     arrow = document.getElementsByClassName("arrow")[0],
     sidebar = document.getElementsByClassName("sidebar")[0],
+    profile_photo = document.getElementsByClassName("profile")[0].firstElementChild,
     profile_name= document.getElementsByClassName("profile")[0].lastElementChild,
     search_input= document.querySelector("#search-input"),
     button_title = document.querySelectorAll('.button > div:nth-child(2)');
@@ -11,7 +12,36 @@ let collapse = document.getElementsByClassName("collapse")[0],
     circle = document.querySelector('#circle'),
     mode_icon = document.querySelector('#mode-icon'),
     root_elements = document.querySelector(':root'),
-    header = document.getElementsByClassName('header')[0];
+    header = document.getElementsByClassName('header')[0],
+    placeholder = header.getAttribute('data-placeholder'),
+    Download = document.getElementsByClassName("link-3")[0],
+    discription = document.querySelector('#discription');
+
+// Download File Content
+Download.addEventListener("click",()=>{
+    let content = "" + header.innerHTML + "\n\n" + discription.value; 
+    console.log(content);
+    const link = document.createElement("a");
+    // Create a blog object with the file content which you want to add to the file
+    const file = new Blob([content], { type: 'text/plain' });
+    // Add file content in the object URL
+    link.href = URL.createObjectURL(file);
+    // Add file name
+    link.download = "Note.txt";
+    // Add click event to <a> tag to save file.
+    link.click();
+})
+
+
+//Header-Text
+if (header.innerHTML === '') {
+    header.innerHTML = placeholder;
+}
+header.addEventListener("click",()=>{
+    if(header.innerHTML === placeholder){
+        header.innerHTML = '';
+    }
+})
 
 
 
@@ -19,6 +49,7 @@ let collapse = document.getElementsByClassName("collapse")[0],
 collapse.addEventListener("click",()=>{
     if(sidebar.style.width === "60px"){
         sidebar.style.width = "300px";
+        profile_photo.innerHTML = "";
         setTimeout(() => {
             profile_name.style.display = "unset";
             search_input.style.display = "unset";
@@ -39,6 +70,7 @@ collapse.addEventListener("click",()=>{
             element.style.display = "none";
             mode_icon.style.display = "none";
         });
+        profile_photo.innerHTML = "J";
         collapse.style.left = "85px";
         container_2.style.width = "91.6%";
         Switch.style.margin = "0 5px";
